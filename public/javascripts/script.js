2e0628b5b8e38e55799b8b804a9121c2b9821d1b
Greg Mackelden <gmackelden@gmail.com>
2013-03-12 15:40:47 +0000

    more stuffs

---
+++ public/javascripts/script.js
@@ -0,0 +1,135 @@
+// jobs array should have been embedded in template...
+
+$(function() {
+
+  // SOME SETUP
+  var MAX_POINTS = 30;
+  var jobs;
+  var skillList = [
+    'mathreason',
+    'programming',
+    'techdesign',
+    'mean',
+    'numbers',
+    'science',
+    'maths',
+    'principal',
+    'ordering',
+    'dedreason'
+  ];
+
+  // character object
+  window.character = (function() {
+    var name = '',
+        points = MAX_POINTS,
+        skills = {},
+        ratios = {},
+        elm = $('#character');
+
+    _.each(skillList, function(skill) {
+      skills[skill] = 0;
+      ratios[skill] = 0;
+    });
+
+    // increment a skill point
+    function skillUp(skill) {
+      if (!points) return false;
+
+      points--;
+      skills[skill]++;
+      _calcRatio(skill);
+    }
+
+    // decrement a skill point
+    function skillDown(skill) {
+      if (points >= MAX_POINTS || skills[skill] <= 0) return false;
+
+      points++;
+      skills[skill]--;
+      _calcRatio(skill);
+    }
+
+    // calculate the ratio of a skill point
+    function _calcRatio(skill) {
+      var val = skills[skill];
+      ratios[skill] = Math.round((val/MAX_POINTS) * 100);
+    }
+
+    // compare the user with a list of jobs
+    function compareJobs(jobs) {
+      _.each(jobs, function(job) {
+        _compareJob(job);
+      });
+    }
+
+    // compare the user with a job
+    // outputs the total points difference (lower == better)
+    function _compareJob(job) {
+      var totalDiff = 0;
+
+      _.each(job.skills, function(skill) {
+        var diff = ratios[skill.name] - skill.ratio;
+            diff = Math.abs(diff);
+            totalDiff += diff;
+      });
+
+      console.log(totalDiff);
+    }
+
+    function render() {
+      if (!elm.length) alert('page doesn\'t look right...');
+
+      // render available job points
+      elm.find('#points span').html(points);
+
+      _.each(skillList, function(skill) {
+        elm.find('.skill.' + skill).find('.level').html(skills[skill]);
+        console.log(ratios[skill]);
+        elm.find('.skill.' + skill).find('.ratio span').html(ratios[skill]);
+      });
+
+      compareJobs(jobs);
+    }
+
+    return {
+      name: name,
+      points: points,
+      skills: skills,
+
+      skillUp: skillUp,
+      skillDown: skillDown,
+
+      render: render
+    };
+  })();
+
+console.log(character);
+
+  // DOM EVENTS
+  $(document).on('click', '.skill .up', function(e) {
+    var skillElm = $(this).parent(),
+        name = skillElm.data('skill');
+
+    character.skillUp(name);
+    character.render();
+  });
+
+  $(document).on('click', '.skill .down', function(e) {
+    var skillElm = $(this).parent(),
+        name = skillElm.data('skill')
+
+    character.skillDown(name);
+    character.render();
+  });
+
+  // INIT CODES
+  $.ajax('/jobs', {
+    method: 'post',
+    success: function(res) {
+      jobs = res;
+
+      character.render();
+    }
+  });
+
+});

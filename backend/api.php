337b2a7c4a47a8948d52076b5e7181240e4e64c4
Giuseppe Sollazzo <puntofisso@gmail.com>
2013-04-24 11:37:47 +0100

    api.php

--- backend/api.php
+++ backend/api.php
@@ -1,6 +1,8 @@
 <?php
 
 $url="https://www.googleapis.com/books/v1/volumes?" ;
+$key="AIzaSyCuXGrFNlf2HsOnyJ2voblBpCwbkuYGHVk";
+
 //q=subject%3Dmathematics"
 
 $character = $_GET['character'];
@@ -10,7 +12,8 @@ $soc = $_GET['soc'];
 $char_array = explode(",", $character);
 $soc_array = explode(",", $soc);
 
-echo $char_array[0];
+
+// 1. calculate the skill set we will searching
 
 
 ?>
